9d4ecf74ebdcaceb44c42358797d7b2e31a22c87
EJBrasil <rivaldo18@gmail.com>
2022-03-14 13:37:47 -0300

    Adicionar arquivos de projeto.

---
+++ PortifolioWebSite/Controllers/DepoimentosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortifolioWebSite.Controllers
+{
+    public class DepoimentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
